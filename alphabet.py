397e9d25c017ea7848f0e25dea0fc0e7ced6ca9d
andreastedile <andrea.stedile@studenti.unitn.it>
2024-08-21 16:14:17 +0200

    define alphabet file

---
+++ alphabet.py
@@ -0,0 +1,13 @@
+INPUT_ALPHABET = [
+    'C_GenerateKey_flag0',
+    'C_GenerateKey_wrapping0',
+    'C_SetAttribute_flag0_SENSITIVE_True',
+    'C_SetAttribute_wrapping0_SENSITIVE_True',
+    'C_SetAttribute_wrapping0_WRAP_True',
+    'C_SetAttribute_wrapping0_WRAP_False',
+    'C_SetAttribute_flag0_EXTRACTABLE_False',
+    'C_SetAttribute_wrapping0_DECRYPT_True',
+    'C_SetAttribute_wrapping0_DECRYPT_False',
+    'C_WrapKey_flag0',
+    'C_Decrypt_wrapped0',
+]
