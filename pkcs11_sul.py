be5a0fc8e63ac1a422bdb637d2d68ea11b5ae66a
andreastedile <andrea.stedile@studenti.unitn.it>
2024-11-28 13:42:01 +0100

    add pkcs11_sul

---
+++ pkcs11_sul.py
@@ -0,0 +1,35 @@
+import typing
+
+from aalpy import SUL
+from pkcs11 import Session, SecretKey, Object
+
+from grammar.inputs import PKCS11_SUL_Input
+
+
+# noinspection PyPep8Naming
+class PKCS11_SUL(SUL):
+    __slots__ = ["session", "initial_knowledge_factory", "knowledge_set"]
+
+    def __init__(self, session: Session,
+                 initial_knowledge_factory: typing.Callable[[Session], dict[int, SecretKey | bytes]]):
+        super().__init__()
+
+        self.session = session
+        for obj in session.get_objects():
+            obj: Object
+            obj.destroy()
+
+        self.initial_knowledge_factory = initial_knowledge_factory
+        self.knowledge_set: dict[int, SecretKey | bytes] = {}
+
+    def pre(self):
+        self.knowledge_set = self.initial_knowledge_factory(self.session)
+
+    def post(self):
+        objects = self.session.get_objects()
+        for obj in objects:
+            obj: Object
+            obj.destroy()
+
+    def step(self, input: PKCS11_SUL_Input):
+        return input.execute(self.knowledge_set)
