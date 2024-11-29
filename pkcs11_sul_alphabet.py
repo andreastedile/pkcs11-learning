ff6765112d7a8201631534c07bb54b8ad7d468c9
andreastedile <andrea.stedile@studenti.unitn.it>
2024-11-29 13:23:43 +0100

    do some useful logging

--- pkcs11_sul_alphabet.py
+++ pkcs11_sul_alphabet.py
@@ -16,9 +16,18 @@ def compute_alphabet(graph: dict[int, HandleNode | KeyNode],
                      blocked_node_ids: set[int] = None,
                      unwrap_func: Callable[[int | None, dict[int, HandleNode | KeyNode]], int] = standard_unwrap_func,
                      debug=False) -> list[PKCS11_SUL_Input]:
+    print("expand graph")
     graph = expand_graph(graph, n_iter, unwrap_func, debug)
+
+    print("number of handle nodes:", len([attr for attr in graph.values() if isinstance(attr, HandleNode)]))
+    print("number of key nodes:   ", len([attr for attr in graph.values() if isinstance(attr, KeyNode)]))
+
+    print("pruning")
     if do_pruning:
         graph = prune_graph(graph, blocked_node_ids, debug)
+
+    print("number of handle nodes:", len([attr for attr in graph.values() if isinstance(attr, HandleNode)]))
+    print("number of key nodes:   ", len([attr for attr in graph.values() if isinstance(attr, KeyNode)]))
 
     alphabet = []
 
@@ -45,5 +54,11 @@ def compute_alphabet(graph: dict[int, HandleNode | KeyNode],
             alphabet.append(PKCS11_SUL_UnsetEncrypt(n))
             alphabet.append(PKCS11_SUL_SetDecrypt(n))
             alphabet.append(PKCS11_SUL_UnsetDecrypt(n))
+
+    print(f"alphabet has {len(alphabet)} inputs, of which:")
+    print("number of wrap inputs:   ", len([input for input in alphabet if isinstance(input, PKCS11_SUL_Wrap)]))
+    print("number of unwrap inputs: ", len([input for input in alphabet if isinstance(input, PKCS11_SUL_Unwrap)]))
+    print("number of encrypt inputs:", len([input for input in alphabet if isinstance(input, PKCS11_SUL_Encrypt)]))
+    print("number of decrypt inputs:", len([input for input in alphabet if isinstance(input, PKCS11_SUL_Decrypt)]))
 
     return alphabet
