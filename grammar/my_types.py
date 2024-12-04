6be78e167f117cf67d42c3083aa477a4df5c2a3f
andreastedile <andrea.stedile@studenti.unitn.it>
2024-12-04 12:52:21 +0100

    document the constructors

--- grammar/my_types.py
+++ grammar/my_types.py
@@ -3,6 +3,13 @@ type KeyType = int | tuple[KeyType, KeyType]
 
 class HandleNode:
     def __init__(self, initial: bool, points_to: int, use: bool, unwrap_in: tuple[int, int] | None):
+        """
+        :param initial: Whether the handle node is part of the initial knowledge. If true, the node cannot be pruned.
+        :param points_to: Key node pointed by the handle node.
+        :param use: Whether the handle node can be used as wrapping key, unwrapping key, encryption key, decryption key.
+        :param unwrap_in: Pair of handle node and key node, the first being the (handle to the) unwrapping key, the
+         second being the wrapped key.
+        """
         self.points_to = points_to
         self.unwrap_in = unwrap_in
         self.use = use
@@ -23,6 +30,21 @@ class KeyNode:
     def __init__(self, initial: bool, value: KeyType, known: bool, handle_in: list[int], wrap_in: list[tuple[int, int]],
                  encrypt_in: list[tuple[int, int]], decrypt_in: list[tuple[int, int]],
                  intruder_decrypt_in: list[tuple[int, int]]):
+        """
+        :param initial: Whether the key node is part of the initial knowledge. If true, the node cannot be pruned.
+        :param value: Value of the key node.
+        :param known: Whether the key node is known and can be used as key to be wrapped, wrapped key, key to be
+        encrypted, key to be decrypted.
+        :param handle_in: Handle nodes pointing to the key node.
+        :param wrap_in: Pairs of handle node and key node, the first being the (handle to the) wrapping key, the second
+         being the key to be wrapped.
+        :param encrypt_in: Pairs of handle node and key node, the first being the (handle to the) encryption key, the
+         second being the key to be encrypted.
+        :param decrypt_in: Pairs of handle node and key node, the first being the (handle to the) decryption key, the
+         second being the key to be decrypted.
+        :param intruder_decrypt_in: Pairs of key node and key node, the first being the decryption key, the second being
+         the key to be decrypted.
+        """
         self.value = value
         self.known = known
         self.handle_in = handle_in
