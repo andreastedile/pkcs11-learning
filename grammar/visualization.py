e8255867c57eba20825e74024c9ff358d6acf92c
andreastedile <andrea.stedile@studenti.unitn.it>
2024-12-16 20:09:59 +0100

    implement model visualization

--- grammar/visualization.py
+++ grammar/visualization.py
@@ -3,7 +3,48 @@ from pydot import Dot, Node, Edge
 from grammar.my_types import HandleNode, KeyNode, Security
 
 
-def convert_graph_to_dot(graph: dict[int, HandleNode | KeyNode]) -> Dot:
+def convert_graph_to_dot(graph: dict[int, HandleNode | KeyNode],
+                         true_nodes: list[int] = None,
+                         visible_wrap_implications: list[tuple[int, int, int]] = None,
+                         visible_unwrap_implications: list[tuple[int, int, int]] = None,
+                         visible_encrypt_implications: list[tuple[int, int, int]] = None,
+                         visible_decrypt_implications: list[tuple[int, int, int]] = None,
+                         visible_intruder_decrypt_implications: list[tuple[int, int, int]] = None) -> Dot:
+    if true_nodes is None:
+        true_nodes = list(graph.keys())
+
+    if visible_wrap_implications is None:
+        visible_wrap_implications = []
+        for n, attr in graph.items():
+            if isinstance(attr, KeyNode):
+                for (e1, e2) in attr.wrap_in:
+                    visible_wrap_implications.append((e1, e2, n))
+    if visible_unwrap_implications is None:
+        visible_unwrap_implications = []
+        for n, attr in graph.items():
+            if isinstance(attr, HandleNode):
+                match attr.unwrap_in:
+                    case (e1, e2):
+                        visible_unwrap_implications.append((e1, e2, n))
+    if visible_encrypt_implications is None:
+        visible_encrypt_implications = []
+        for n, attr in graph.items():
+            if isinstance(attr, KeyNode):
+                for (e1, e2) in attr.encrypt_in:
+                    visible_encrypt_implications.append((e1, e2, n))
+    if visible_decrypt_implications is None:
+        visible_decrypt_implications = []
+        for n, attr in graph.items():
+            if isinstance(attr, KeyNode):
+                for (e1, e2) in attr.decrypt_in:
+                    visible_decrypt_implications.append((e1, e2, n))
+    if visible_intruder_decrypt_implications is None:
+        visible_intruder_decrypt_implications = []
+        for n, attr in graph.items():
+            if isinstance(attr, KeyNode):
+                for (e1, e2) in attr.intruder_decrypt_in:
+                    visible_intruder_decrypt_implications.append((e1, e2, n))
+
     dot = Dot(graph_type="digraph", labeljust="right")
 
     for n, attr in graph.items():
@@ -12,7 +53,7 @@ def convert_graph_to_dot(graph: dict[int, HandleNode | KeyNode]) -> Dot:
                 n,
                 label=f"id={n}, points_to={attr.points_to}",
                 shape="box",
-                style="filled",
+                style="filled" if n in true_nodes else "invis",
                 fillcolor="lightblue",
                 peripheries=2 if attr.initial else 1)
             dot.add_node(node)
@@ -21,7 +62,7 @@ def convert_graph_to_dot(graph: dict[int, HandleNode | KeyNode]) -> Dot:
                 n,
                 label=f"id={n}, value={attr.value}",
                 shape="box",
-                style="filled",
+                style="filled" if n in true_nodes else "invis",
                 fillcolor="lightgreen",
                 peripheries=2 if attr.initial else 1,
                 penwidth=1 if attr.security == Security.LOW else 2.5)
@@ -33,30 +74,52 @@ def convert_graph_to_dot(graph: dict[int, HandleNode | KeyNode]) -> Dot:
                 case (e1, e2):
                     edge1 = Edge(e1, n, label=f"<unwrap(<u>{e1}</u>,{e2})>")
                     edge2 = Edge(e2, n, label=f"<unwrap({e1},<u>{e2}</u>)>")
+                    if (e1, e2, n) not in visible_unwrap_implications:
+                        edge1.set_style("invis")
+                    if (e1, e2, n) not in visible_unwrap_implications:
+                        edge2.set_style("invis")
                     dot.add_edge(edge1)
                     dot.add_edge(edge2)
         elif isinstance(attr, KeyNode):
             for e in attr.handle_in:
                 edge = Edge(e, n, label="handle")
+                if n not in true_nodes or e not in true_nodes:
+                    edge.set_style("invis")
                 dot.add_edge(edge)
             for (e1, e2) in attr.wrap_in:
                 edge1 = Edge(e1, n, label=f"<wrap(<u>{e1}</u>,{e2})>")
                 edge2 = Edge(e2, n, label=f"<wrap({e1},<u>{e2}</u>)>")
+                if (e1, e2, n) not in visible_wrap_implications:
+                    edge1.set_style("invis")
+                if (e1, e2, n) not in visible_wrap_implications:
+                    edge2.set_style("invis")
                 dot.add_edge(edge1)
                 dot.add_edge(edge2)
             for (e1, e2) in attr.encrypt_in:
                 edge1 = Edge(e1, n, label=f"<encrypt(<u>{e1}</u>,{e2})>")
                 edge2 = Edge(e2, n, label=f"<encrypt({e1},<u>{e2}</u>)>")
+                if (e1, e2, n) not in visible_encrypt_implications:
+                    edge1.set_style("invis")
+                if (e1, e2, n) not in visible_encrypt_implications:
+                    edge2.set_style("invis")
                 dot.add_edge(edge1)
                 dot.add_edge(edge2)
             for (e1, e2) in attr.decrypt_in:
                 edge1 = Edge(e1, n, label=f"<decrypt(<u>{e1}</u>,{e2})>")
                 edge2 = Edge(e2, n, label=f"<decrypt({e1},<u>{e2}</u>)>")
+                if (e1, e2, n) not in visible_decrypt_implications:
+                    edge1.set_style("invis")
+                if (e1, e2, n) not in visible_decrypt_implications:
+                    edge2.set_style("invis")
                 dot.add_edge(edge1)
                 dot.add_edge(edge2)
             for (e1, e2) in attr.intruder_decrypt_in:
                 edge1 = Edge(e1, n, label=f"<intruderdecrypt(<u>{e1}</u>,{e2})>")
                 edge2 = Edge(e2, n, label=f"<intruderdecrypt({e1},<u>{e2}</u>)>")
+                if (e1, e2, n) not in visible_intruder_decrypt_implications:
+                    edge1.set_style("invis")
+                if (e1, e2, n) not in visible_intruder_decrypt_implications:
+                    edge2.set_style("invis")
                 dot.add_edge(edge1)
                 dot.add_edge(edge2)
 
--- model_checking/graph_sat.py
+++ model_checking/graph_sat.py
@@ -1,76 +1,84 @@
+import re
+
 import mathsat
 from pysmt.fnode import FNode
 from pysmt.shortcuts import *
 from pysmt.solvers.msat import MathSAT5Solver
 
 from grammar.my_types import HandleNode, KeyNode
+from grammar.visualization import convert_graph_to_dot
 
 
 def run_sat(graph: dict[int, HandleNode | KeyNode], high_security_node: int, print_models=False):
     high_security_node_attr = graph[high_security_node]
     assert isinstance(high_security_node_attr, KeyNode)
     assert len(high_security_node_attr.decrypt_in) > 0 or len(high_security_node_attr.intruder_decrypt_in) > 0
-    print(graph[0])
 
     assertions = []
 
     for n, attr in graph.items():
+        print(n, attr)
+
         if isinstance(attr, HandleNode):
-            if attr.initial:
-                pass
-            else:
-                match attr.unwrap_in:
-                    case (e1, e2):
-                        iif = Iff(Symbol(str(n)), Symbol(f"unwrap({e1},{e2})={n}"))
-                        assertions.append(iif)
-                        impl = Implies(Symbol(f"unwrap({e1},{e2})={n}"),
-                                       And(Symbol(str(e1)), Symbol(str(e2))))
-                        assertions.append(impl)
-        elif isinstance(attr, KeyNode):
-            if attr.initial and attr.copy.known:
-                pass
-            else:
-                if attr.is_implied_by_other_nodes():
-                    implications = []
-                    for (e1, e2) in attr.wrap_in:
-                        implications.append(Symbol(f"wrap({e1},{e2})={n}"))
-                    for (e1, e2) in attr.encrypt_in:
-                        implications.append(Symbol(f"encrypt({e1},{e2})={n}"))
-                    for (e1, e2) in attr.decrypt_in:
-                        implications.append(Symbol(f"decrypt({e1},{e2})={n}"))
-                    for (e1, e2) in attr.intruder_decrypt_in:
-                        implications.append(Symbol(f"intruder_decrypt({e1},{e2})={n}"))
-
-                    iif = Iff(Symbol(str(n)), Or(implications))
-                    assertions.append(iif)
-
-                    amo = AtMostOne(implications)
-                    if amo != TRUE():  # so that it does not appear when printing assertions
-                        assertions.append(amo)
+            match attr.unwrap_in:
+                case (e1, e2) if not attr.initial:
+                    iff = Iff(Symbol(str(n)), Symbol(f"unwrap({e1},{e2})={n}"))
+                    print(iff)
+                    assertions.append(iff)
 
                     # a command requires both its operands
-                    for (e1, e2) in attr.wrap_in:
-                        impl = Implies(Symbol(f"wrap({e1},{e2})={n}"),
-                                       And(Symbol(str(e1)), Symbol(str(e2))))
-                        assertions.append(impl)
-                    for (e1, e2) in attr.encrypt_in:
-                        impl = Implies(Symbol(f"encrypt({e1},{e2})={n}"),
-                                       And(Symbol(str(e1)), Symbol(str(e2))))
-                        assertions.append(impl)
-                    for (e1, e2) in attr.decrypt_in:
-                        impl = Implies(Symbol(f"decrypt({e1},{e2})={n}"),
-                                       And(Symbol(str(e1)), Symbol(str(e2))))
-                        assertions.append(impl)
-                    for (e1, e2) in attr.intruder_decrypt_in:
-                        impl = Implies(Symbol(f"intruder_decrypt({e1},{e2})={n}"),
-                                       And(Symbol(str(e1)), Symbol(str(e2))))
-                        assertions.append(impl)
+                    impl = Implies(Symbol(f"unwrap({e1},{e2})={n}"),
+                                   And(Symbol(str(e1)), Symbol(str(e2)), Symbol(str(n))))
+                    print(impl)
+                    assertions.append(impl)
+                case None:
+                    assert attr.initial
+                    # can be freely satisfied
+        elif isinstance(attr, KeyNode):
+            if attr.initial and attr.copy.known:
+                # can be freely satisfied
+                pass
+            else:
+                implications = []
+                for (e1, e2) in attr.wrap_in:
+                    implications.append(Symbol(f"wrap({e1},{e2})={n}"))
+                for (e1, e2) in attr.encrypt_in:
+                    implications.append(Symbol(f"encrypt({e1},{e2})={n}"))
+                for (e1, e2) in attr.decrypt_in:
+                    implications.append(Symbol(f"decrypt({e1},{e2})={n}"))
+                for (e1, e2) in attr.intruder_decrypt_in:
+                    implications.append(Symbol(f"intruder_decrypt({e1},{e2})={n}"))
+
+                iif = Iff(Symbol(str(n)), Or(implications))
+                print(iif)
+                assertions.append(iif)
+
+                amo = AtMostOne(implications)
+                assertions.append(amo)
+
+                # a command requires both its operands
+                for (e1, e2) in attr.wrap_in:
+                    impl = Implies(Symbol(f"wrap({e1},{e2})={n}"),
+                                   And(Symbol(str(e1)), Symbol(str(e2)), Symbol(str(n))))
+                    print(impl)
+                    assertions.append(impl)
+                for (e1, e2) in attr.encrypt_in:
+                    impl = Implies(Symbol(f"encrypt({e1},{e2})={n}"),
+                                   And(Symbol(str(e1)), Symbol(str(e2)), Symbol(str(n))))
+                    print(impl)
+                    assertions.append(impl)
+                for (e1, e2) in attr.decrypt_in:
+                    impl = Implies(Symbol(f"decrypt({e1},{e2})={n}"),
+                                   And(Symbol(str(e1)), Symbol(str(e2)), Symbol(str(n))))
+                    print(impl)
+                    assertions.append(impl)
+                for (e1, e2) in attr.intruder_decrypt_in:
+                    impl = Implies(Symbol(f"intruder_decrypt({e1},{e2})={n}"),
+                                   And(Symbol(str(e1)), Symbol(str(e2)), Symbol(str(n))))
+                    print(impl)
+                    assertions.append(impl)
 
     assertions.append(Symbol(str(high_security_node)))
-
-    print(f"{len(assertions)} assertions:")
-    for assertion in assertions:
-        print(assertion)
 
     formula: FNode = And(assertions)
 
@@ -98,5 +106,52 @@ def run_sat(graph: dict[int, HandleNode | KeyNode], high_security_node: int, pri
         print(f"found {len(models)} models")
         if print_models:
             for i, model in enumerate(models):
-                py_model_true_nodes = [node for node in model if not node.is_not()]
-                print(f"Model {i + 1}: {py_model_true_nodes}")
+                py_model_true_nodes = [node for node in model if node.is_symbol()]  # same as if not node.is_not()
+                print(f"Model {i}: {py_model_true_nodes}")
+
+        prog = re.compile(r"^(?:\d+|(wrap|unwrap|encrypt|decrypt|intruder_decrypt)\((\d+),(\d+)\)=(\d+))$")
+
+        for i, model in enumerate(models):
+            visible_nodes = []
+            visible_wrap_implications = []
+            visible_unwrap_implications = []
+            visible_encrypt_implications = []
+            visible_decrypt_implications = []
+            visible_intruder_decrypt_implications = []
+
+            for atom in model:
+                if atom.is_symbol():
+                    name: str = atom.symbol_name()
+                    match = re.match(prog, name)
+                    if match:
+                        if match.group(1) is None:
+                            visible_nodes.append(int(name))
+                        else:
+                            command, param1, param2, result = match.groups()
+                            match command:
+                                case "wrap":
+                                    visible_wrap_implications.append((int(param1), int(param2), int(result)))
+                                case "unwrap":
+                                    visible_unwrap_implications.append((int(param1), int(param2), int(result)))
+                                case "encrypt":
+                                    visible_encrypt_implications.append((int(param1), int(param2), int(result)))
+                                case "decrypt":
+                                    visible_decrypt_implications.append((int(param1), int(param2), int(result)))
+                                case "intruder_decrypt":
+                                    visible_intruder_decrypt_implications.append(
+                                        (int(param1), int(param2), int(result)))
+                                case other:
+                                    raise ValueError(other)
+                    else:
+                        print("Input does not match the pattern:", atom.symbol_name())
+                else:
+                    assert atom.is_not()
+
+            dot = convert_graph_to_dot(graph,
+                                       visible_nodes,
+                                       visible_wrap_implications,
+                                       visible_unwrap_implications,
+                                       visible_encrypt_implications,
+                                       visible_decrypt_implications,
+                                       visible_intruder_decrypt_implications)
+            dot.write(f"model_{i}.svg", format="svg")
