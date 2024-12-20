1905e6a152dcfba1df5633759fa79ce4ca2cf7ec
andreastedile <andrea.stedile@studenti.unitn.it>
2024-12-20 13:57:24 +0100

    update convert_model_to_alphabet

--- pkcs11_sul_alphabet.py
+++ pkcs11_sul_alphabet.py
@@ -1,11 +1,10 @@
-import re
-
 from pysmt.fnode import FNode
 
 from pkcs11_sul_inputs import PKCS11_SUL_Input, \
     PKCS11_SUL_Wrap, PKCS11_SUL_Unwrap, PKCS11_SUL_Encrypt, PKCS11_SUL_Decrypt, PKCS11_SUL_IntruderDecrypt, \
     PKCS11_SUL_SetWrap, PKCS11_SUL_SetUnwrap, PKCS11_SUL_SetEncrypt, PKCS11_SUL_SetDecrypt, \
-    PKCS11_SUL_UnsetWrap, PKCS11_SUL_UnsetUnwrap, PKCS11_SUL_UnsetEncrypt, PKCS11_SUL_UnsetDecrypt
+    PKCS11_SUL_UnsetWrap, PKCS11_SUL_UnsetUnwrap, PKCS11_SUL_UnsetEncrypt, PKCS11_SUL_UnsetDecrypt, \
+    convert_str_input_to_pkcs11_sul_input
 from grammar.my_types import HandleNode, KeyNode
 
 
@@ -52,48 +51,26 @@ def extract_alphabet(graph: dict[int, HandleNode | KeyNode]) -> list[PKCS11_SUL_
 
 
 def convert_model_to_alphabet(graph: dict[int, HandleNode | KeyNode], model: list[FNode]) -> list[PKCS11_SUL_Input]:
-    prog = re.compile(r"^(?:\d+|(wrap|unwrap|encrypt|decrypt|intruder_decrypt)\((\d+),(\d+)\)=(\d+))$")
-
-    alphabet = []
+    alphabet: list[PKCS11_SUL_Input] = []
 
     for atom in model:
         if atom.is_symbol():
             name: str = atom.symbol_name()
-            match = re.match(prog, name)
-            if match:
-                if match.group(1) is None:
-                    n = int(name)
-                    attr = graph[n]
-                    if isinstance(attr, HandleNode):
-                        alphabet.append(PKCS11_SUL_SetWrap(n))
-                        alphabet.append(PKCS11_SUL_UnsetWrap(n))
-                        alphabet.append(PKCS11_SUL_SetUnwrap(n))
-                        alphabet.append(PKCS11_SUL_UnsetUnwrap(n))
-                        alphabet.append(PKCS11_SUL_SetEncrypt(n))
-                        alphabet.append(PKCS11_SUL_UnsetEncrypt(n))
-                        alphabet.append(PKCS11_SUL_SetDecrypt(n))
-                        alphabet.append(PKCS11_SUL_UnsetDecrypt(n))
-                else:
-                    command, param1, param2, result = match.groups()
-                    command: str
-                    param1: str
-                    param2: str
-                    result: str
-                    match command:
-                        case "wrap":
-                            alphabet.append(PKCS11_SUL_Wrap(int(param1), int(param2), int(result)))
-                        case "unwrap":
-                            alphabet.append(PKCS11_SUL_Unwrap(int(param1), int(param2), int(result)))
-                        case "encrypt":
-                            alphabet.append(PKCS11_SUL_Encrypt(int(param1), int(param2), int(result)))
-                        case "decrypt":
-                            alphabet.append(PKCS11_SUL_Decrypt(int(param1), int(param2), int(result)))
-                        case "intruder_decrypt":
-                            alphabet.append(PKCS11_SUL_IntruderDecrypt(int(param1), int(param2), int(result)))
-                        case other:
-                            raise ValueError(other)
+            if name.isdigit():
+                n = int(name)
+                attr = graph[n]
+                if isinstance(attr, HandleNode):
+                    alphabet.append(PKCS11_SUL_SetWrap(n))
+                    alphabet.append(PKCS11_SUL_UnsetWrap(n))
+                    alphabet.append(PKCS11_SUL_SetUnwrap(n))
+                    alphabet.append(PKCS11_SUL_UnsetUnwrap(n))
+                    alphabet.append(PKCS11_SUL_SetEncrypt(n))
+                    alphabet.append(PKCS11_SUL_UnsetEncrypt(n))
+                    alphabet.append(PKCS11_SUL_SetDecrypt(n))
+                    alphabet.append(PKCS11_SUL_UnsetDecrypt(n))
             else:
-                print("Input does not match the pattern:", atom.symbol_name())
+                converted = convert_str_input_to_pkcs11_sul_input(name)
+                alphabet.append(converted)
         else:
             assert atom.is_not()
 
