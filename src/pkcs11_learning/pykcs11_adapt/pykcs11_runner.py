3dfd73ac993a94106073375efb41733621e03059
andreastedile <andrea.stedile@unitn.it>
2025-06-19 15:04:52 +0200

    Make run return a custom type

--- src/pkcs11_learning/pykcs11_adapt/pykcs11_runner.py
+++ src/pkcs11_learning/pykcs11_adapt/pykcs11_runner.py
@@ -5,13 +5,14 @@ from PyKCS11 import PyKCS11Lib, Session
 from PyKCS11.LowLevel import CKF_RW_SESSION
 
 
-def run(lib: PyKCS11Lib, so: str, slot: int, pin: str, code: typing.Callable[[Session], None]):
+def run[T](lib: PyKCS11Lib, so: str, slot: int, pin: str, code: typing.Callable[[Session], T]) -> T | None:
     lib.load(so)
     session = lib.openSession(slot, CKF_RW_SESSION)
     session.login(pin)
 
+    t = None
     try:
-        code(session)
+        t = code(session)
     except Exception as e:  # whatever exception occurs, logout and close the session
         print(e, file=sys.stderr)
         print("logout and close session")
@@ -19,3 +20,5 @@ def run(lib: PyKCS11Lib, so: str, slot: int, pin: str, code: typing.Callable[[Se
     session.logout()
     session.closeSession()
     lib.unload()
+
+    return t
